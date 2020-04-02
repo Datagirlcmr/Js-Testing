38f4886bb0132dcbaddab57aadffb75454f7ad67
Selma Datagirl <datagirlcmr@gmail.com>
2020-04-02 11:04:57 -0500

    implemented the reversedString test

---
+++ src/reversedString/reversedString.test.js
@@ -0,0 +1,11 @@
+import reverseString from "./reversedString"
+
+describe("reverseString",()=>{
+  it("It reverses a string from end to start",()=>{
+    expect(reverseString("selma")).toBe("amles")
+    expect(reverseString("fruits")).toBe("stiurf")
+    expect(reverseString("computer")).toBe("retupmoc")
+    expect(reverseString("phone")).toBe("enohp")
+    expect(reverseString("phone Case")).toBe("esaC enohp")
+  })
+})
