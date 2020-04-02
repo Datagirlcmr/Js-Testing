204a2d68690c236678e495015520a7d3c77b5eef
Selma Datagirl <datagirlcmr@gmail.com>
2020-04-02 15:45:59 -0500

    implemented the ceasar cypher and test

--- src/ceaserCipher/ceasar.js
+++ src/ceaserCipher/ceasar.js
@@ -1,13 +1,13 @@
-export default function cCipher(string) {
+export default function cCipher(string, increm=1) {
     const strarray = string.split('');
     let encryptArr = [];
 
     strarray.forEach((letter, i) => {
-        if (letter == '') {
-            encryptArr.push('');
+        if (letter == ' ') {
+            encryptArr.push(' ');
         }
         else {
-            let character = string.charCodeAt(i) + 1;
+            let character = string.charCodeAt(i) + increm;
             const newLetter = String.fromCharCode(character);
             encryptArr.push(newLetter);
         }
--- src/ceaserCipher/ceasar.test.js
+++ src/ceaserCipher/ceasar.test.js
@@ -1,8 +1,15 @@
-import Ccipher from './ceasar';
+import cCipher from './ceasar';
 
 describe('Ccipher', ()=>{
-    it('encrypts a string', ()=>{
-        expect(Ccipher('abcdef')).toBe('bcdefg')
+    it('encrypts a string by default', ()=>{
+        expect(cCipher('abcdef')).toBe('bcdefg')
+    })
 
+    it('returns an empty space when there is no letter ', () =>{
+        expect (cCipher('abcdef abcdef')). toBe('bcdefg bcdefg')
+    })
+
+    it('encrypts a string by the increment', ()=>{
+        expect(cCipher('attack at dawn', 5)).toBe('fyyfhp fy ifbs')
     })
 })
