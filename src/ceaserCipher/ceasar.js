99ec8e33ccf168e8a5c1db02b14b04a44e5524aa
Edemone <edem.agbenyo@gmail.com>
2020-04-02 23:56:31 +0000

    Working on ceasar cipher test

--- src/ceaserCipher/ceasar.js
+++ src/ceaserCipher/ceasar.js
@@ -1,17 +1,33 @@
-export default function cCipher(string, increm=1) {
-    const strarray = string.split('');
-    let encryptArr = [];
+export default function cCipher(string, increm = 1) {
+  const strarray = string.split("");
+  let encryptArr = [];
 
-    strarray.forEach((letter, i) => {
-        if (letter == ' ') {
-            encryptArr.push(' ');
-        }
-        else {
-            let character = string.charCodeAt(i) + increm;
-            const newLetter = String.fromCharCode(character);
-            encryptArr.push(newLetter);
-        }
-    });
-    return encryptArr.join('')
+  strarray.forEach((letter, i) => {
+    if (letter == " ") {
+      encryptArr.push(" ");
+    } else {
+      //a = 97 A= 65
+      //z = 122 Z= 90
+      let newCode = string.charCodeAt(i) + increm; // 102
+        let newLetter=null;
+      if (newCode >= 65 && newCode <= 90) {
+          if(newCode >= 97 && newCode <= 122 ){
+              newLetter = String.fromCharCode(newCode);
+          }
+      } else {
+        newCode = 65 + (increm - 1);
+        newLetter = String.fromCharCode(newCode);
+        
+        newCode = 97 + (increm - 1); // 69
+        newLetter = String.fromCharCode(newCode);
+        continue;
+      }
+
+      console.log(newCode);
+      
+    }
+    encryptArr.push(newLetter);
+
+  });
+  return encryptArr.join("");
 }
-
--- src/ceaserCipher/ceasar.test.js
+++ src/ceaserCipher/ceasar.test.js
@@ -2,18 +2,20 @@ import cCipher from './ceasar';
 
 describe('Ccipher', ()=>{
     it('encrypts a string by default', ()=>{
-        expect(cCipher('abcdef')).toBe('bcdefg')
+        expect(cCipher('z',5)).toBe('e')
+        expect(cCipher('Z',5)).toBe('E')
+        expect(cCipher('a',5)).toBe('f')
     })
 
-    it('returns an empty space when there is no letter ', () =>{
-        expect (cCipher('abcdef abcdef')). toBe('bcdefg bcdefg')
-    })
+    // it('returns an empty space when there is no letter ', () =>{
+    //     expect (cCipher('abcdef abcdef')). toBe('bcdefg bcdefg')
+    // })
 
-    it('encrypts a string by the increment', ()=>{
-        expect(cCipher('attack at dawn', 5)).toBe('fyyfhp fy ifbs')
-    })
+    // it('encrypts a string by the increment', ()=>{
+    //     expect(cCipher('attack at dawn', 5)).toBe('fyyfhp fy ifbs')
+    // })
 
-    it('encrypts a string by the increment', ()=>{
-        expect(cCipher('attack at dawn', 5)).toBe('fyyfhp fy ifbs')
-    })
+    // it('encrypts a string by the increment', ()=>{
+    //     expect(cCipher('attack at dawn', 5)).toBe('fyyfhp fy ifbs')
+    // })
 })
